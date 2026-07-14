49fc624acdd4ae9d4985283899b7148b0f745fe7
kollivenu143-byte <kollivenu143@gmail.com>
2026-07-14 21:12:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Products from "./pages/Products";
+
+function App() {
+  return (
+    <div>
+      <h1>ShopEZ</h1>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
